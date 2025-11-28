51fb4895f033c5a22558e97acb86bab7cff7adb1
julius_Calvin <juliuscalvin_kurniadi@apps.ipb.ac.id>
2025-11-28 10:12:37 +0700

    Delete : layout folder containing header and footer is not needed

--- layout/footer.php
+++
@@ -1,9 +0,0 @@
-</div>
-<footer class="text-center mt-5 py-4 bg-light text-muted border-top">
-    <p class="mb-0">&copy; 2025 Julius Calvin - Sistem Taksi</p>
-</footer>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-
-</html>
